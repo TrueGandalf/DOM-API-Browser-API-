5461802f0a607e0130d1120b2a7c8cc2a7b311f5
Oleksii Zhyglov <OleksiiZhyglov@gmail.com>
2018-07-09 23:30:44 +0300

    10 posts work

--- main.js
+++ main.js
@@ -10,9 +10,46 @@ if (xhr.status != 200) {
 } else {
 	dataFromJson = xhr.responseText;
 }
+const objectFromJson = JSON.parse(dataFromJson);
+//alert(objectFromJson);
 
-alert(dataFromJson);
+console.log(objectFromJson);
 
-//alert( '-----------');
+const div = document.getElementById('container');
 
-alert( xhr.responseText );
+//alert(div.innerHTML);
+
+
+
+for (let i = 0; i < 10; i++) {
+
+	//console.log(objectFromJson);
+	/*console.log(objectFromJson.data[i].title);*/
+
+
+	const post = document.createElement('div');
+	//post.innerHTML = objectFromJson[i];
+	div.appendChild(post);
+
+	const title = document.createElement("span");
+	title.innerHTML = objectFromJson.data[i].title;
+	post.appendChild(title);
+
+	const img = document.createElement("img");
+	img.src = objectFromJson.data[i].image;
+	post.appendChild(img);
+
+	const description = document.createElement("div");
+	description.innerHTML = objectFromJson.data[i].description;
+	post.appendChild(description);
+
+	const createdAt = document.createElement("div");
+	createdAt.innerHTML = objectFromJson.data[i].createdAt;
+	post.appendChild(createdAt);
+
+	const tags = document.createElement("div");
+	tags.innerHTML = objectFromJson.data[i].tags.join(" ");
+	post.appendChild(tags);
+
+
+}
